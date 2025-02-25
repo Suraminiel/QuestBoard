7ada0837d1e45e4b1d19b3fb22f743944e2861db
Suraminiel <Suraminiel@gmx.de>
2025-02-25 17:19:01 +0100

    Add project files.

---
+++ QuestBoard/Controllers/QuestController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace QuestBoard.Controllers
+{
+    public class QuestController : Controller
+    {
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Overview()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Calender()
+        {
+            return View();
+        }
+    }
+}
